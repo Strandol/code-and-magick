3e3010e0bbaa78d37ccf19ae7add710a8ba8bc6e
Strandol <dvalian@mail.ru>
2016-11-23 01:33:13 +0200

    Cookies

--- build/js/form.js
+++ build/js/form.js
@@ -1,9 +1,64 @@
 'use strict';
 
 (function() {
+  var docCookies = {
+    getItem: function (sKey) {
+      return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    },
+    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+      var sExpires = "";
+      if (vEnd) {
+        switch (vEnd.constructor) {
+          case Number:
+            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+            break;
+          case String:
+            sExpires = "; expires=" + vEnd;
+            break;
+          case Date:
+            sExpires = "; expires=" + vEnd.toUTCString();
+            break;
+        }
+      }
+      document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+      return true;
+    },
+
+
+    removeItem: function (sKey, sPath, sDomain) {
+      if (!sKey || !this.hasItem(sKey)) { return false; }
+      document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + ( sDomain ? "; domain=" + sDomain : "") + ( sPath ? "; path=" + sPath : "");
+      return true;
+    },
+    hasItem: function (sKey) {
+      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    },
+    keys: /* optional method: you can safely remove it! */ function () {
+      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+      for (var nIdx = 0; nIdx < aKeys.length; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+      return aKeys;
+    }
+  };
+
+  var form = document.querySelector('.review-form');
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  var formName = document.querySelector('#review-name');
+  var formReview = document.querySelector('#review-text');
+  var formNameLabel = document.querySelector('.review-fields-name');
+  var formReviewLabel = document.querySelector('.review-fields-text');
+
+  var birthday = new Date(1997, 12, 23);
+
+  if(docCookies.hasItem(formName.name)){
+    formName.value = docCookies.getItem(formName.name);
+  }
+
+  if(docCookies.hasItem(formReview.name)){
+    formReview.value = docCookies.getItem(formReview.name);
+  }
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
@@ -14,4 +69,32 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  formName.oninput = function (evt) {
+    evt.preventDefault();
+    if(formName.value === '') {
+      formNameLabel.classList.remove('invisible');
+    } else {
+      formNameLabel.classList.add('invisible');
+    }
+  };
+
+  formReview.oninput = function (evt) {
+    evt.preventDefault();
+    if(formReview.value === '') {
+      formReviewLabel.classList.remove('invisible');
+    } else {
+      formReviewLabel.classList.add('invisible');
+    }
+  };
+
+  form.onsubmit = function (evt) {
+    evt.preventDefault();
+
+    docCookies.setItem(formName.name, formName.value, new Date(Date.now() + (Date.now() - birthday.getTime())));
+    docCookies.setItem(formReview.name, formReview.value, new Date(Date.now() + (Date.now() - birthday.getTime())));
+
+    this.submit();
+  };
+
 })();
