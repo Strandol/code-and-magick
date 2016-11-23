7daf1892b069d9f6822719be46bd3af8732a2c4a
Strandol <dvalian@mail.ru>
2016-11-24 01:42:27 +0200

    Refactoring

--- build/js/form.js
+++ build/js/form.js
@@ -1,45 +1,7 @@
 'use strict';
 
 (function() {
-  var docCookies = {
-    getItem: function (sKey) {
-      return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-    },
-    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-      var sExpires = "";
-      if (vEnd) {
-        switch (vEnd.constructor) {
-          case Number:
-            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-            break;
-          case String:
-            sExpires = "; expires=" + vEnd;
-            break;
-          case Date:
-            sExpires = "; expires=" + vEnd.toUTCString();
-            break;
-        }
-      }
-      document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-      return true;
-    },
-
-
-    removeItem: function (sKey, sPath, sDomain) {
-      if (!sKey || !this.hasItem(sKey)) { return false; }
-      document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + ( sDomain ? "; domain=" + sDomain : "") + ( sPath ? "; path=" + sPath : "");
-      return true;
-    },
-    hasItem: function (sKey) {
-      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-    },
-    keys: /* optional method: you can safely remove it! */ function () {
-      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-      for (var nIdx = 0; nIdx < aKeys.length; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
-      return aKeys;
-    }
-  };
+  var MY_BIRTHDAY = new Date(1997, 12, 23);
 
   var form = document.querySelector('.review-form');
   var formContainer = document.querySelector('.overlay-container');
@@ -50,51 +12,46 @@
   var formNameLabel = document.querySelector('.review-fields-name');
   var formReviewLabel = document.querySelector('.review-fields-text');
 
-  var birthday = new Date(1997, 12, 23);
+  CheckCookie(formName);
+  CheckCookie(formReview);
 
-  if(docCookies.hasItem(formName.name)){
-    formName.value = docCookies.getItem(formName.name);
-  }
-
-  if(docCookies.hasItem(formReview.name)){
-    formReview.value = docCookies.getItem(formReview.name);
-  }
-
-  formOpenButton.onclick = function(evt) {
-    evt.preventDefault();
+  formOpenButton.onclick = function(event) {
+    event.preventDefault();
     formContainer.classList.remove('invisible');
   };
 
-  formCloseButton.onclick = function(evt) {
-    evt.preventDefault();
+  formCloseButton.onclick = function(event) {
+    event.preventDefault();
     formContainer.classList.add('invisible');
   };
 
-  formName.oninput = function (evt) {
-    evt.preventDefault();
-    if(formName.value === '') {
-      formNameLabel.classList.remove('invisible');
-    } else {
-      formNameLabel.classList.add('invisible');
-    }
+  formName.oninput = function (event) {
+    event.preventDefault();
+    CheckValue(formName, formNameLabel);
   };
 
-  formReview.oninput = function (evt) {
-    evt.preventDefault();
-    if(formReview.value === '') {
-      formReviewLabel.classList.remove('invisible');
-    } else {
-      formReviewLabel.classList.add('invisible');
-    }
+  formReview.oninput = function (event) {
+    event.preventDefault();
+    CheckValue(formReview, formReviewLabel);
   };
 
-  form.onsubmit = function (evt) {
-    evt.preventDefault();
+  form.onsubmit = function (event) {
+    event.preventDefault();
 
-    docCookies.setItem(formName.name, formName.value, new Date(Date.now() + (Date.now() - birthday.getTime())));
-    docCookies.setItem(formReview.name, formReview.value, new Date(Date.now() + (Date.now() - birthday.getTime())));
+    docCookies.setItem(formName.name, formName.value, new Date(Date.now() + (Date.now() - MY_BIRTHDAY.getTime())));
+    docCookies.setItem(formReview.name, formReview.value, new Date(Date.now() + (Date.now() - MY_BIRTHDAY.getTime())));
 
     this.submit();
   };
+
+  function CheckCookie(formElement) {
+    if(docCookies.hasItem(formElement.name)){
+      formElement.value = docCookies.getItem(formElement.name);
+    }
+  }
+
+  function CheckValue(formElement, elementLabel) {
+    formElement.value === '' ? elementLabel.classList.remove('invisible') : elementLabel.classList.add('invisible');
+  }
 
 })();
