8d318b901439c4ff563ed62e10be3ace8856fdf9
Strandol <dvalian@mail.ru>
2016-12-06 21:22:07 +0200

    Module4-task1

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,4 @@
+<<<<<<< 388806ad59ed7d4bc165cc3bd9c784f8a85d56b8
 'use strict';
 
 (function() {
@@ -141,3 +142,69 @@
 
     window.Gallery = Gallery;
 }());
+=======
+'use strict';
+
+(function() {
+    var KEY_CODE = {
+        'ESC': 27,
+        'BUTTON_RIGHT': 39,
+        'BUTTON_LEFT': 37
+    };
+
+    var galleryContainer = document.querySelector('.photogallery');
+    var galleryOverlay = document.querySelector('.overlay-gallery');
+    var galleryCloseBtn = document.querySelector('.overlay-gallery-close');
+
+    galleryContainer.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var element = event.target;
+        while (element) {
+            if (element.classList.contains('photogallery-image')) {
+                showGalleryOverlay();
+                setCloseBtnHandler();
+                setKeyHandlers();
+                return;
+            }
+
+            element = element.parentElement;
+        }
+    });
+
+    function showGalleryOverlay() {
+        galleryOverlay.classList.remove('invisible');
+    }
+
+    function setKeyHandlers() {
+        window.addEventListener('keyup', keyHandlers);
+    }
+    
+    function setCloseBtnHandler() {
+        galleryCloseBtn.addEventListener('click', hideGallery);
+    }
+
+    var hideGallery = function() {
+        galleryOverlay.classList.add('invisible');
+        window.removeEventListener('keyup', keyHandlers);
+    };
+
+    function keyHandlers(event) {
+        event.preventDefault();
+
+        switch (event.keyCode) {
+            case KEY_CODE.ESC:
+                galleryOverlay.classList.add('invisible');
+                break;
+            case KEY_CODE.BUTTON_LEFT:
+                console.log('LEFT');
+                break;
+            case KEY_CODE.BUTTON_RIGHT:
+                console.log('RIGHT');
+                break;
+            default:
+                break;
+        }
+    }
+}());
+>>>>>>> Module4-task1
--- js/game_demo.js
+++ js/game_demo.js
@@ -3,14 +3,20 @@
 (function() {
     var clouds = document.querySelector('.header-clouds');
 
+<<<<<<< 388806ad59ed7d4bc165cc3bd9c784f8a85d56b8
     var timerId = 0;
     var cloudsPositionX = 0;
 
     var moveClouds = new Event('move');
+=======
+
+    var moveClouds = new CustomEvent('move');
+>>>>>>> Module4-task1
     
     clouds.addEventListener('move', move);
 
     function move() {
+<<<<<<< 388806ad59ed7d4bc165cc3bd9c784f8a85d56b8
         cloudsPositionX = clouds.getBoundingClientRect().top;
         clouds.style.backgroundPosition = cloudsPositionX + 'px 0px';
     }
@@ -22,6 +28,18 @@
             } else {
                 clouds.addEventListener('move', move);
             }
+=======
+        clouds.style.backgroundPosition = clouds.getBoundingClientRect().top  + (document.documentElement.clientWidth / 4) + 'px 0px';
+        console.log('move');
+    }
+
+    window.addEventListener('scroll', function(event) {
+        var timerId = setTimeout(function() {
+            clouds.getBoundingClientRect().bottom < 0
+                ? clouds.removeEventListener('move', move)
+                : clouds.addEventListener('move', move);
+            clearTimeout(timerId);
+>>>>>>> Module4-task1
         }, 100);
 
         clouds.dispatchEvent(moveClouds);
